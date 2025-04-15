3ccbb371fca91129310a69514522bc8c1e95cb71
Rodrigo-28 <rodrigoromano2019@gmail.com>
2025-04-15 20:58:53 -0300

    task

--- ExercisesCode/Program.cs
+++ ExercisesCode/Program.cs
@@ -132,33 +132,82 @@ Console.WriteLine("Hello, World!");
 
 
 
-static char FindMissingLetter(char[] array)
-{
-    // con linq
-    //char[] arrayMinuscula = array.Select(c => char.ToLower(c)).ToArray();
-    //char inicio = array[0];
+//static char FindMissingLetter(char[] array)
+//{
+//    // con linq
+//    //char[] arrayMinuscula = array.Select(c => char.ToLower(c)).ToArray();
+//    //char inicio = array[0];
 
-    //List<char> list = new List<char>();
+//    //List<char> list = new List<char>();
 
-    //List<char> abecedario = Enumerable.Range(inicio, 26)
-    //                    .Select(x => (char)x)
-    //                    .ToList();
-    //for (int i = 0; i < array.Length; i++)
-    //{
-    //    if (array[i] != abecedario[i])
-    //    {
-    //        list.Add(abecedario[i]);
-    //        break;
-    //    }
-    //}
+//    //List<char> abecedario = Enumerable.Range(inicio, 26)
+//    //                    .Select(x => (char)x)
+//    //                    .ToList();
+//    //for (int i = 0; i < array.Length; i++)
+//    //{
+//    //    if (array[i] != abecedario[i])
+//    //    {
+//    //        list.Add(abecedario[i]);
+//    //        break;
+//    //    }
+//    //}
 
 
-    //return list[0];
+//    //return list[0];
 
-    // Se genera la secuencia esperada del primer al último elemento + 1 elemento (por la letra faltante).
-    var expected = Enumerable.Range(array[0], array.Length + 1)
-                             .Select(x => (char)x);
-    // Except devuelve la diferencia (la letra que falta).
-    return expected.Except(array).Single();
-}
-Console.WriteLine(FindMissingLetter(['a', 'b', 'c', 'd', 'f']).ToString());
+//    // Se genera la secuencia esperada del primer al último elemento + 1 elemento (por la letra faltante).
+//    var expected = Enumerable.Range(array[0], array.Length + 1)
+//                             .Select(x => (char)x);
+//    // Except devuelve la diferencia (la letra que falta).
+//    return expected.Except(array).Single();
+//}
+//Console.WriteLine(FindMissingLetter(['a', 'b', 'c', 'd', 'f']).ToString());
+
+
+//****************  Task ****************
+
+//static string Rgb(int r, int g, int b)
+//{
+//    //int valorMax = 255;
+//    //int valorMin = 0;
+
+//    //string rojo = MaxMin(r, valorMax, valorMin).ToString("X2");
+//    //string verde = MaxMin(g, valorMax, valorMin).ToString("X2");
+//    //string azul = MaxMin(b, valorMax, valorMin).ToString("X2");
+
+//    //return rojo + verde + azul;
+//    // OPCION DOS
+
+//    return ToHex(r) + ToHex(g) + ToHex(b);
+
+//}
+
+//static int MaxMin(int n, int valorMax, int valorMin)
+//{
+//    if (n > valorMax)
+//    {
+//        n = 255;
+//    }
+//    if (n < valorMin)
+//    {
+//        n = 0;
+//    }
+//    return n;
+//}
+
+//static string ToHex(int value)
+//{
+//    if (value < 0) value = 0;
+//    if (value > 255) value = 255;
+//    return value.ToString("X2");
+//}
+
+//*************** TASK      ****************/
+//Crea una función que reciba como parámetro un número entero positivo entre 1 y 3999 (ambos inclusive)
+//y que devuelva una cadena de texto con la representación en números romanos de ese número.
+
+//Los números romanos modernos se escriben expresando cada dígito por separado, empezando
+//por el dígito más a la izquierda, y omitendo cualquier dígito que tenga valor cero.
+//Además, no puede haber más de 3 símbolos romanos idénticos seguidos.
+
+
