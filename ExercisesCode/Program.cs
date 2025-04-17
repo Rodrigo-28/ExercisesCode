8ba60e1d3c9b68f9852736bfe7624d028f27e100
Rodrigo-28 <rodrigoromano2019@gmail.com>
2025-04-16 21:19:02 -0300

    task

--- ExercisesCode/Program.cs
+++ ExercisesCode/Program.cs
@@ -211,3 +211,52 @@ Console.WriteLine("Hello, World!");
 //Además, no puede haber más de 3 símbolos romanos idénticos seguidos.
 
 
+static string Solution(int n)
+{
+    //string resultado = "";
+    //int resta = n;
+    //int[] valores = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    //string[] simbolos = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+
+    //for (int i = 0; i < valores.Length; i++)
+    //{
+    //    while (resta >= valores[i])
+    //    {
+    //        resultado += simbolos[i];
+    //        resta -= valores[i];
+    //    }
+    //}
+    //return resultado;
+
+    // con diccionario
+    var mapa = new Dictionary<int, string>()
+    {
+        {1000, "M"},
+        {900, "CM"},
+        {500, "D"},
+        {400, "CD"},
+        {100, "C"},
+        {90, "XC"},
+        {50, "L"},
+        {40, "XL"},
+        {10, "X"},
+        {9, "IX"},
+        {5, "V"},
+        {4, "IV"},
+        {1, "I"}
+    };
+    string resultado = "";
+    foreach (var par in mapa)
+    {
+        while (n >= par.Key)
+        {
+            resultado += par.Value;
+            n -= par.Key;
+        }
+    }
+
+    return resultado;
+}
+
+Console.WriteLine(Solution(1987));
